9c378458d36f1ab369c54f86f0f9081441443e2e
Shubham Singh <ishubhamsingh2e@gmail.com>
2023-02-12 06:45:15 +0530

    added: cross-origin

--- api/app.py
+++ api/app.py
@@ -10,8 +10,21 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from hashlib import md5
 
+from fastapi.middleware.cors import CORSMiddleware
+
 CWD = Path.cwd()
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 BUILD_FILE = "build.json"
 SECRET = "secret.json"
 
